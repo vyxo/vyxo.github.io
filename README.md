8bff40b820d2005ac3aa235f1748e887f9c12c45
Vyxo <100800443+vyxo@users.noreply.github.com>
2022-03-17 19:21:54 +0000

    updated theme, configured in-page links, changed banner

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# vyxo.github.io
-My static website!
+# Vyxo
+## About
+I am just *another* web developer in this universe. Other than a bit of **JavaScript** knowledge, I am pretty useless. You can find me on [GitHub](https://github.com/vyxo/), [Discord](https://discord.gg/T53wbpsScu) and some other sites!
+
+## Support
+If you liked this website or you just want to be nice feel free to follow me on [GitHub](https://github.com/vyxo/) and join the [Discord](https://discord.gg/T53wbpsScu) server. I appreciate new members!
+
+> I might start a YouTube channel soon. It doesn't sound like a bad idea...
+
+## Usage
+If you plan on using the JS or CSS of this site **please give credit** because it took a *lot* of work to figure out the smooth scrolling, loading effects, hover effects, and animations.
--- script.js
+++ script.js
@@ -1,58 +1,69 @@
-const fonts = document.getElementsByClassName('f')
+const fades = document.getElementsByClassName('f')
 const headers = document.getElementsByClassName('smooth')
 
 setTimeout(() => {
-  let elems = fonts.length
+  let elems = fades.length
   for (i = 0; i < elems; i++) {
-    fonts[0].classList.remove('f')
+    fades[0].classList.remove('f')
   }
 }, 1500)
 
-const body = document.body;
-const app = document.getElementById('app');
+for (header of headers) {
+  const target = header.getAttribute('href')
+  const el = document.querySelector(target)
+  if (el) {
+    header.addEventListener('click', () => {
+      console.log(el.offsetTop)
+      window.scroll(0, el.offsetTop)
+    })
+  }
+}
+
+const body = document.body
+const app = document.getElementById('app')
 
 let sx = 0, // For scroll positions
-    sy = 0;
+    sy = 0
 let dx = sx, // For container positions
-    dy = sy;
+    dy = sy
+
+body.style.height = app.clientHeight + 'px'
 
 
-body.style.height = app.clientHeight + 'px';
-
-
-app.style.position = 'fixed';
-app.style.top = 0;
-app.style.left = 0;
+app.style.position = 'fixed'
+app.style.top = 0
+app.style.left = 0
 
 // Bind a scroll function
-window.addEventListener('scroll', easeScroll);
+// window.addEventListener('scroll', easeScroll)
 
 
 function easeScroll() {
-  sx = window.pageXOffset;
-  sy = window.pageYOffset;
+  sx = window.pageXOffset
+  sy = window.pageYOffset
 }
 
-
-window.requestAnimationFrame(render);
-
-function render(){
+function render() {
   //We calculate our container position by linear interpolation method
-  body.style.height = app.clientHeight + 'px';
-  dx = li(dx,sx,0.1);
-  dy = li(dy,sy,0.1);
+  body.style.height = app.clientHeight + 'px'
+  dx = li(dx,sx,0.1)
+  dy = li(dy,sy,0.1)
   
-  dx = Math.floor(dx * 100) / 100;
-  dy = Math.floor(dy * 100) / 100;
-  
-  
-  app.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
- 
+  dx = Math.floor(dx * 100) / 100
+  dy = Math.floor(dy * 100) / 100
   
   
-  window.requestAnimationFrame(render);
+  app.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`
 }
 
 function li(a, b, n) {
-  return (1 - n) * a + n * b;
+  return (1 - n) * a + n * b
 }
+
+function scrollbar() {
+  easeScroll()
+  render()
+  window.requestAnimationFrame(scrollbar)
+}
+
+window.requestAnimationFrame(scrollbar)
